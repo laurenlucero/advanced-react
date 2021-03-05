c535466f92744bb1c1804ff22b6b2bbefdad9165
“Lauren <lucerolaurenm@gmail.com>
2021-03-05 16:20:19 -0500

    Style Header

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,15 +1,48 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const LogoStyled = styled.h1`
+  background: red;
+  font-size: 4rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyled = styled.header`
+  .bar {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    border-bottom: 10px solid var(--black, black);
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    border-bottom: 10px solidvar(--black, black);
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <div className="bar">
-        <Link href="/">Sick Fits Logo</Link>
+        <LogoStyled>
+          <Link href="/">Sick Fits Logo</Link>
+        </LogoStyled>
       </div>
-      <div className="sub-bar" />
-      <p>Search</p>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
       <Nav />
-    </header>
+    </HeaderStyled>
   );
 }
