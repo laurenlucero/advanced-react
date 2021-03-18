9a7a98f2ff291b7ecff8bba3592bb998bdff0b92
“Lauren <lucerolaurenm@gmail.com>
2021-03-18 16:03:51 -0400

    Creating products via mutations

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,12 +1,1 @@
 export { default } from './products';
-
-// import Page from '../components/Page';
-
-// export default function IndexPage() {
-//   return (
-//     <>
-//       <p>Hello, Index</p>
-//       <code>var</code>
-//     </>
-//   );
-// }
